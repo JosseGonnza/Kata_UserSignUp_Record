15c7564567bac6006830dfe08c408829540ebb61
JosseGonnza <accion.quevedo@hotmail.com>
2024-08-16 15:33:26 +0100

    ♻️ Restart Kata

---
+++ Kata_UserSignUp_Record/Controllers/WeatherForecastController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace Kata_UserSignUp_Record.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+}
