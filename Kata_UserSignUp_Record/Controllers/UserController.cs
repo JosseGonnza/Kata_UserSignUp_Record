637972d30f8eb076b06e09092285b3144eec303b
JosseGonnza <accion.quevedo@hotmail.com>
2024-08-16 19:28:52 +0100

    ✨ Add new method Post and Get in UserController, and create fakeRepository.

--- Kata_UserSignUp_Record/Controllers/UserController.cs
+++ Kata_UserSignUp_Record/Controllers/UserController.cs
@@ -1,7 +1,3 @@
-using Kata_UserSignUp_Record.Models;
-using Kata_UserSignUp_Record.Repositories;
-using Kata_UserSignUp_Record.ValueObjects;
-using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kata_UserSignUp_Record.Controllers;
@@ -10,4 +6,38 @@ namespace Kata_UserSignUp_Record.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
+    private readonly FakeRepository repository;
+
+    public UserController(FakeRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUser(UserRequest request)
+    {
+        User user = new User(Guid.NewGuid(), request.Email, request.Password);
+        repository.Save(user);
+        return base.Accepted(user);
+    }
+
+    [HttpGet]
+    public async Task<List<User>> GetAllUsers()
+    {
+        return repository.GetAll();
+    }
+
 }
+
+public class FakeRepository
+{
+    private List<User> users = new List<User>();
+
+    public void Save(User user) => users.Add(user);
+
+    public List<User> GetAll() => users;
+}
+
+public record User(Guid Id, string Email, string Password);
+
+public record UserRequest(string Email, string Password);
